5b9308e8dd61f84ed9c6d01ff04c8f8c33137cfb
Ansariel <devnull@localhost>
2015-10-09 09:26:21 +0200

    STORM-2122: UDP data bandwidth measuring is off by factor 8

--- indra/newview/llviewerregion.cpp
+++ indra/newview/llviewerregion.cpp
@@ -1770,7 +1770,7 @@ void LLViewerRegion::updateNetStats()
 	mLastPacketsLost =	mPacketsLost;
 
 	mPacketsIn =				cdp->getPacketsIn();
-	mBitsIn =					8 * cdp->getBytesIn();
+	mBitsIn =					cdp->getBytesIn();
 	mPacketsOut =				cdp->getPacketsOut();
 	mPacketsLost =				cdp->getPacketsLost();
 	mPingDelay =				cdp->getPingDelay();
