e6f79c71b154a4eeb91227c0b05b64b738c7b292
Drake Arconis <drake@alchemyviewer.org>
2015-10-30 23:57:24 -0400

    Remove the private memory pool and clean up related code and menu items

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -798,7 +798,6 @@ bool LLAppViewer::init()
 	initMaxHeapSize() ;
 	LLCoros::instance().setStackSize(gSavedSettings.getS32("CoroutineStackSize"));
 
-	LLPrivateMemoryPoolManager::initClass((BOOL)gSavedSettings.getBOOL("MemoryPrivatePoolEnabled"), (U32)gSavedSettings.getU32("MemoryPrivatePoolSize")*1024*1024) ;
 	// write Google Breakpad minidump files to a per-run dump directory to avoid multiple viewer issues.
 	std::string logdir = gDirUtilp->getExpandedFilename(LL_PATH_DUMP, "");
 	mDumpPath = logdir;
@@ -1335,10 +1334,6 @@ bool LLAppViewer::mainLoop()
 	LLTimer frameTimer,idleTimer;
 	LLTimer debugTime;
 	
-	//LLPrivateMemoryPoolTester::getInstance()->run(false) ;
-	//LLPrivateMemoryPoolTester::getInstance()->run(true) ;
-	//LLPrivateMemoryPoolTester::destroy() ;
-
 	// Handle messages
 #ifdef LL_DARWIN
 	if (!LLApp::isExiting())
@@ -2119,9 +2114,6 @@ bool LLAppViewer::cleanup()
 	LLWearableType::cleanupClass();
 
 	LLMainLoopRepeater::instance().stop();
-
-	//release all private memory pools.
-	LLPrivateMemoryPoolManager::destroyClass() ;
 
 	ll_close_fail_log();
 
