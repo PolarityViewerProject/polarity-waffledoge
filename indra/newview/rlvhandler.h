6dea67caba3d591ff7292439f9132a853027a1e2
Drake Arconis <drake@alchemyviewer.org>
2016-01-31 21:30:42 -0500

    Maybe fix the build

--- indra/newview/rlvhandler.h
+++ indra/newview/rlvhandler.h
@@ -20,9 +20,7 @@
 #include <stack>
 
 #include "rlvcommon.h"
-#if LL_GNUC
-#include "rlvhelper.h"		// Needed to make GCC happy
-#endif // LL_GNUC
+#include "rlvhelper.h"
 
 // ============================================================================
 
