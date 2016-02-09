d12d6b2375f34bf022bddd653697632dd14e52e8
Drake Arconis <drake@alchemyviewer.org>
2016-02-09 10:39:29 -0500

    Add initial GLM package

--- indra/cmake/LLRender.cmake
+++ indra/cmake/LLRender.cmake
@@ -3,11 +3,13 @@
 include(Variables)
 include(FreeType)
 include(GLH)
+include(GLM)
 include(GLEW)
 
 set(LLRENDER_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llrender
     ${GLH_INCLUDE_DIR}
+	${GLM_INCLUDE_DIR}
     )
 
 if (BUILD_HEADLESS)
