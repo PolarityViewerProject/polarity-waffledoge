ab6d0cce94d350dfe5d711a035e62ca49b6c4e43
Drake Arconis <drake@alchemyviewer.org>
2016-02-13 05:57:55 -0500

    Remove GLH build files

--- indra/cmake/LLRender.cmake
+++ indra/cmake/LLRender.cmake
@@ -2,13 +2,11 @@
 
 include(Variables)
 include(FreeType)
-include(GLH)
 include(GLM)
 include(GLEW)
 
 set(LLRENDER_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llrender
-    ${GLH_INCLUDE_DIR}
 	${GLM_INCLUDE_DIR}
     )
 
