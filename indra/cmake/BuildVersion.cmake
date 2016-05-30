0116c03a98509c59e743c103c7c606d8ea23ee9f
Xenhat Liamano <xenhat@polarityviewer.org>
2016-05-30 13:28:32 -0400

    Backed out changeset: 41c2de5614b9 (Date based version stuff)

--- indra/cmake/BuildVersion.cmake
+++ indra/cmake/BuildVersion.cmake
@@ -2,51 +2,51 @@
 # Construct the viewer version number based on the indra/VIEWER_VERSION file
 
 if (NOT DEFINED VIEWER_SHORT_VERSION) # will be true in indra/, false in indra/newview/
-	if(DEFINED VIEWER_BUILD_VERSION)
-      set(VIEWER_SHORT_VERSION ${VIEWER_BUILD_VERSION})
-    else(DEFINED VIEWER_BUILD_VERSION)
-      string(TIMESTAMP VIEWER_SHORT_VERSION "%y.%m.%d" UTC)
-    endif(DEFINED VIEWER_BUILD_VERSION)
-		
-    string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+" "\\1" VIEWER_VERSION_MAJOR ${VIEWER_SHORT_VERSION})
-    string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+" "\\1" VIEWER_VERSION_MINOR ${VIEWER_SHORT_VERSION})
-    string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)" "\\1" VIEWER_VERSION_PATCH ${VIEWER_SHORT_VERSION})
+    set(VIEWER_VERSION_BASE_FILE "${CMAKE_CURRENT_SOURCE_DIR}/newview/VIEWER_VERSION.txt")
 
-    if (DEFINED ENV{revision})
-       set(VIEWER_VERSION_REVISION $ENV{revision})
-       message("Revision (from environment): ${VIEWER_VERSION_REVISION}")
+    if ( EXISTS ${VIEWER_VERSION_BASE_FILE} )
+        file(STRINGS ${VIEWER_VERSION_BASE_FILE} VIEWER_SHORT_VERSION REGEX "^[0-9]+\\.[0-9]+\\.[0-9]+")
+        string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+" "\\1" VIEWER_VERSION_MAJOR ${VIEWER_SHORT_VERSION})
+        string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+" "\\1" VIEWER_VERSION_MINOR ${VIEWER_SHORT_VERSION})
+        string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)" "\\1" VIEWER_VERSION_PATCH ${VIEWER_SHORT_VERSION})
 
-    else (DEFINED ENV{revision})
-      find_program(MERCURIAL
-                   NAMES hg
-                   PATHS [HKEY_LOCAL_MACHINE\\Software\\TortoiseHG]
-                   PATH_SUFFIXES Mercurial)
-      mark_as_advanced(MERCURIAL)
-      if (MERCURIAL)
-        execute_process(COMMAND ${MERCURIAL} identify -n
-                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-                        RESULT_VARIABLE hg_id_result
-                        ERROR_VARIABLE hg_id_error
-                        OUTPUT_VARIABLE VIEWER_VERSION_REVISION
-                        OUTPUT_STRIP_TRAILING_WHITESPACE)
-        if (NOT ${hg_id_result} EQUAL 0)
-          message(SEND_ERROR "Revision number generation failed with output:\n${hg_id_error}")
-        else (NOT ${hg_id_result} EQUAL 0)
-          string(REGEX REPLACE "[^0-9a-f]" "" VIEWER_VERSION_REVISION ${VIEWER_VERSION_REVISION})
-        endif (NOT ${hg_id_result} EQUAL 0)
-        if ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
-          message("Revision (from hg) ${VIEWER_VERSION_REVISION}")
-        else ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
-          message("Revision not set (repository not found?); using 0")
-          set(VIEWER_VERSION_REVISION 0 )
-        endif ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
-       else (MERCURIAL)
-          message("Revision not set: mercurial not found; using 0")
-          set(VIEWER_VERSION_REVISION 0)
-       endif (MERCURIAL)
-    endif (DEFINED ENV{revision})
+        if (DEFINED ENV{revision})
+           set(VIEWER_VERSION_REVISION $ENV{revision})
+           message("Revision (from environment): ${VIEWER_VERSION_REVISION}")
 
-    message("Building '${VIEWER_CHANNEL}' Version ${VIEWER_SHORT_VERSION}.${VIEWER_VERSION_REVISION}")
+        else (DEFINED ENV{revision})
+          find_program(MERCURIAL
+                       NAMES hg
+                       PATHS [HKEY_LOCAL_MACHINE\\Software\\TortoiseHG]
+                       PATH_SUFFIXES Mercurial)
+          mark_as_advanced(MERCURIAL)
+          if (MERCURIAL)
+            execute_process(COMMAND ${MERCURIAL} identify -n
+                            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                            RESULT_VARIABLE hg_id_result
+                            ERROR_VARIABLE hg_id_error
+                            OUTPUT_VARIABLE VIEWER_VERSION_REVISION
+                            OUTPUT_STRIP_TRAILING_WHITESPACE)
+            if (NOT ${hg_id_result} EQUAL 0)
+              message(SEND_ERROR "Revision number generation failed with output:\n${hg_id_error}")
+            else (NOT ${hg_id_result} EQUAL 0)
+              string(REGEX REPLACE "[^0-9a-f]" "" VIEWER_VERSION_REVISION ${VIEWER_VERSION_REVISION})
+            endif (NOT ${hg_id_result} EQUAL 0)
+            if ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
+              message("Revision (from hg) ${VIEWER_VERSION_REVISION}")
+            else ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
+              message("Revision not set (repository not found?); using 0")
+              set(VIEWER_VERSION_REVISION 0 )
+            endif ("${VIEWER_VERSION_REVISION}" MATCHES "^[0-9]+$")
+           else (MERCURIAL)
+              message("Revision not set: mercurial not found; using 0")
+              set(VIEWER_VERSION_REVISION 0)
+           endif (MERCURIAL)
+        endif (DEFINED ENV{revision})
+        message("Building '${VIEWER_CHANNEL}' Version ${VIEWER_SHORT_VERSION}.${VIEWER_VERSION_REVISION}")
+    else ( EXISTS ${VIEWER_VERSION_BASE_FILE} )
+        message(SEND_ERROR "Cannot get viewer version from '${VIEWER_VERSION_BASE_FILE}'") 
+    endif ( EXISTS ${VIEWER_VERSION_BASE_FILE} )
 
     if ("${VIEWER_VERSION_REVISION}" STREQUAL "")
       message("Ultimate fallback, revision was blank or not set: will use 0")
