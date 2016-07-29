dc0a5a44552aa03e9024a72a298d64481bdf0ed0
Xenhat Liamano <xenhat@polarityviewer.org>
2016-07-29 02:57:33 -0400

    Fixed: Default role color references a non-existing color table entry

--- indra/newview/llinspectavatar.cpp
+++ indra/newview/llinspectavatar.cpp
@@ -402,7 +402,7 @@ void LLInspectAvatar::onAvatarNameCache(
 		if (!agent_role_text.empty())
 		{
 			agent_role->setValue(agent_role_text);
-			agent_role->setColor(PVData::instance().getColor(mAvatarID, LLUIColorTable::instance().getColor("DefaultTextColor"), false));
+			agent_role->setColor(PVData::instance().getColor(mAvatarID, LLColor4::white, false));
 		}
 
 	}
