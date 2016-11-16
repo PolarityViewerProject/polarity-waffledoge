ed79a5ea464cb6fa396a936985064768cb88f948
Xenhat Liamano <xenhat@polarityviewer.org>
2016-11-16 16:10:41 -0500

    82ecd871e787 part 2: It helps to include the files.

---
+++ indra/newview/pvgpuinfo.cpp
@@ -0,0 +1,71 @@
+/**
+* @file FILE pvgpuinfo.cpp
+* @brief Class containing useful information about the Graphic processor in use
+*
+* $LicenseInfo:firstyear=2015&license=viewerlgpl$
+* Polarity Viewer Source Code
+* Copyright (C) 2016 Xenhat Liamano
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation;
+* version 2.1 of the License only.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* The Polarity Viewer Project
+* http://www.polarityviewer.org
+* $/LicenseInfo$
+*/
+
+#include "llviewerprecompiledheaders.h" // ewwww...
+#include "llgl.h"
+#include "pvgpuinfo.h"
+
+S32Megabytes PVGPUInfo::vram_available_mb = S32Megabytes(0);
+S32Megabytes PVGPUInfo::vram_in_use_mb = S32Megabytes(0);
+
+PVGPUInfo::PVGPUInfo()
+{
+
+	GLint memInfo; // in KB
+	if (gGLManager.mIsATI)
+	{
+		glGetIntegerv(GL_VBO_FREE_MEMORY_ATI, &memInfo);
+		vram_available_mb = S32Kilobytes(memInfo);
+	}
+	else if (gGLManager.mIsNVIDIA)
+	{
+		glGetIntegerv(GL_GPU_MEMORY_INFO_CURRENT_AVAILABLE_VIDMEM_NVX, &memInfo);
+		vram_available_mb = S32Kilobytes(memInfo);
+	}
+	else
+	{
+		// The Intel driver cannot reliably know how much memory is in use,
+		// let's assume there is no free vram and skip drawing the grey bar.
+		vram_available_mb = S32Kilobytes(128); // 0 causes fun bugs.
+	}
+	vram_in_use_mb = vram_available_mb - vram_available_mb;
+}
+
+S32 PVGPUInfo::getTotalVRAMS32()
+{
+	// return existing variable to avoid memory bloat
+	return gGLManager.mVRAM;
+}
+
+bool PVGPUInfo::hasEnoughVRAMForSnapshot(const S32 tentative_x, const S32 tentative_y)
+{
+	S32 tentative_pixel_count = tentative_x * tentative_y;
+	if (tentative_pixel_count > (gGLManager.mGLMaxTextureSize * 2) || tentative_pixel_count > getAvailableVRAM().value())
+	{
+		// fallback to something
+		LL_WARNS() << "Available VRAM is smaller than the requested texture size ( " << tentative_x << " x " << tentative_y << ")!" << LL_ENDL;
+		return false;
+	}
+
+	return true;
+}
---
+++ indra/newview/pvgpuinfo.h
@@ -0,0 +1,60 @@
+/**
+* @file FILE pvgpuinfo.h
+* @brief Class containing useful information about the Graphic processor in use
+*
+* $LicenseInfo:firstyear=2015&license=viewerlgpl$
+* Polarity Viewer Source Code
+* Copyright (C) 2016 Xenhat Liamano
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation;
+* version 2.1 of the License only.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* The Polarity Viewer Project
+* http://www.polarityviewer.org
+* $/LicenseInfo$
+*/
+
+#pragma once
+
+//#include "llerror.h"
+#include "llsingleton.h"
+#include "llunits.h"
+
+class PVGPUInfo : public LLSingleton<PVGPUInfo>
+{
+public:
+	static S32Megabytes getAvailableVRAM()
+	{
+		return vram_available_mb;
+	}
+
+	static S32Megabytes getInUseVRAM()
+	{
+		return vram_in_use_mb;
+	}
+
+	static bool hasEnoughVRAMForSnapshot(const S32 tentative_x, const S32 tentative_y);
+
+	/**
+	 * \brief Wrapper around gGLManager.mVRAM
+	 * \return S32
+	 */
+	static S32 getTotalVRAMS32();
+
+	
+	PVGPUInfo();
+
+	// Enables logging for this class
+	typedef PVGPUInfo _LL_CLASS_TO_LOG;
+
+private:
+	static S32Megabytes vram_available_mb;
+	static S32Megabytes vram_in_use_mb;
+};
