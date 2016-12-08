c773a487b00d569e7780c4fa417f5ebca7e87cb6
Drake Arconis <drake@alchemyviewer.org>
2016-12-07 19:57:52 -0500

    Small cleanup of if statements and unused var in llappearance

--- indra/llappearance/llpolymesh.cpp
+++ indra/llappearance/llpolymesh.cpp
@@ -633,13 +633,10 @@ BOOL LLPolyMeshSharedData::loadMesh( const std::string& fileName )
                                         mMorphData.insert(clone_morph_param_cleavage(morph_data,
                                                                                      .75f,
                                                                                      "Breast_Physics_LeftRight_Driven"));
+										mMorphData.insert(clone_morph_param_duplicate(morph_data,
+																					  "Breast_Physics_InOut_Driven"));
                                 }
 
-                                if (!strcmp(morphName, "Breast_Female_Cleavage"))
-                                {
-                                        mMorphData.insert(clone_morph_param_duplicate(morph_data,
-										      "Breast_Physics_InOut_Driven"));
-                                }
                                 if (!strcmp(morphName, "Breast_Gravity"))
                                 {
                                         mMorphData.insert(clone_morph_param_duplicate(morph_data,
@@ -672,12 +669,9 @@ BOOL LLPolyMeshSharedData::loadMesh( const std::string& fileName )
                                         mMorphData.insert(clone_morph_param_direction(morph_data,
 										      LLVector3(0,0,0.05f),
 										      "Butt_Physics_UpDown_Driven"));
-                                }
-                                if (!strcmp(morphName, "Small_Butt"))
-                                {
-                                        mMorphData.insert(clone_morph_param_direction(morph_data,
-										      LLVector3(0,0.03f,0),
-										      "Butt_Physics_LeftRight_Driven"));
+										mMorphData.insert(clone_morph_param_direction(morph_data,
+											LLVector3(0, 0.03f, 0),
+											"Butt_Physics_LeftRight_Driven"));
                                 }
                         }
 
