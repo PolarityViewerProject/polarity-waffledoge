45141832c34cf6e0c58bd4ec32d3e00af046ac7d
Xenhat Liamano <xenhat@polarityviewer.org>
2017-04-20 00:29:34 -0400

    Update boost and depending packages. Credit: Alchemy Viewer

--- indra/cmake/Boost.cmake
+++ indra/cmake/Boost.cmake
@@ -18,7 +18,7 @@ if (USESYSTEMLIBS)
 else (USESYSTEMLIBS)
   use_prebuilt_binary(boost)
   set(Boost_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include)
-  set(BOOST_VERSION "1.60.0")
+  set(BOOST_VERSION "1.63.0")
 
   if (WINDOWS)
     set(BOOST_CHRONO_LIBRARY
