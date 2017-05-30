b01abf2a207f3fe75bcefb587ef9367ad36ed5d5
Xenhat Liamano <xenhat@polarityviewer.org>
2017-05-30 18:52:48 -0400

    std:: isn't a fucking variable name.

--- indra/newview/llinventoryfilter.cpp
+++ indra/newview/llinventoryfilter.cpp
@@ -726,7 +726,7 @@ void LLInventoryFilter::setFilterSubString(const std::string& string)
 		// <polarity> Make inventory search behave like a keyword list instead of a litteral expression
 		// TODO: Add "whole word" option.
 		// TODO: Debug broken first search when separator is not "set" manually
-		// std:: search_separator = PVSearchUtil::getInstance()->getSearchSeparator();
+		// std::string search_separator = PVSearchUtil::getInstance()->getSearchSeparator();
 		static const std:: search_separator = '+';
 		std::string::size_type to;
 
