9d2bc6a1b80a4ca179ce0b4dff87117f0cb56bd3
Xenhat Liamano <xenhat@polarityviewer.org>
2017-05-30 20:34:18 -0400

    Fixed: re-definition of default parameter

--- indra/viewer_components/updater/llupdaterservice.cpp
+++ indra/viewer_components/updater/llupdaterservice.cpp
@@ -718,7 +718,7 @@ void LLUpdaterService::initialize(const std::string & channel,
 								  const std::string & platform_version,
 								  const bool&         willing_to_test,
 								  const unsigned char       uniqueid[MD5HEX_STR_SIZE],
-								  const std::string& auth_token = ""
+								  const std::string& auth_token
 )
 {
 	mImpl->initialize(channel,
