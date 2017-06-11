2cb164363049f91e1bdcec0579070b8b0173af8e
Xenhat Liamano <xenhat@polarityviewer.org>
2017-06-11 11:03:43 -0400

    Fix typo creating a LLSD argument named "Polarity" instead of APP_NAME

--- indra/newview/llmutelist.cpp
+++ indra/newview/llmutelist.cpp
@@ -243,7 +243,7 @@ BOOL LLMuteList::add(const LLMute& mute, U32 flags)
 		if (pv_agent && pv_agent->isProviderDeveloper())
 		{
 			LLSD args;
-			args[APP_NAME] = APP_NAME;
+			args["APP_NAME"] = APP_NAME;
 			LLNotifications::instance().add("MuteDeveloper", LLSD(), args);
 			return FALSE;
 		}
