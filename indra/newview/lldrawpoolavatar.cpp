521aa987618aec8826cda15eb2791edc44b0e278
Xenhat Liamano <xenhat@polarityviewer.org>
2017-06-21 15:03:28 -0400

    Add BT_ALPHA blend thingy from alchemy. plz don't break.

--- indra/newview/lldrawpoolavatar.cpp
+++ indra/newview/lldrawpoolavatar.cpp
@@ -1397,7 +1397,7 @@ void LLDrawPoolAvatar::renderAvatars(LLVOAvatar* single_avatar, S32 pass)
 				renderRigged(avatarp, RIGGED_SPECMAP_BLEND);
 				renderRigged(avatarp, RIGGED_NORMMAP_BLEND);
 				renderRigged(avatarp, RIGGED_NORMSPEC_BLEND);
-				//gGL.setSceneBlendType(LLRender::BT_ALPHA);
+				gGL.setSceneBlendType(LLRender::BT_ALPHA); // <alchemy/>
 				gGL.setColorMask(true, false);
 			}
 			return;
@@ -2127,7 +2127,7 @@ void LLDrawPoolAvatar::renderRiggedAlpha(LLVOAvatar* avatar)
 						LLRender::BF_ONE_MINUS_SOURCE_ALPHA);
 
 		renderRigged(avatar, RIGGED_ALPHA);
-		//gGL.setSceneBlendType(LLRender::BT_ALPHA);
+		gGL.setSceneBlendType(LLRender::BT_ALPHA); // <alchemy/>
 		gGL.setColorMask(true, false);
 	}
 }
@@ -2145,7 +2145,7 @@ void LLDrawPoolAvatar::renderRiggedFullbrightAlpha(LLVOAvatar* avatar)
 						LLRender::BF_ONE_MINUS_SOURCE_ALPHA);
 
 		renderRigged(avatar, RIGGED_FULLBRIGHT_ALPHA);
-		//gGL.setSceneBlendType(LLRender::BT_ALPHA);
+		gGL.setSceneBlendType(LLRender::BT_ALPHA); // <alchemy/>
 		gGL.setColorMask(true, false);
 	}
 }
