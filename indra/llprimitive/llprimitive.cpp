5f1fecac99c57c6ad870d250df5426efe75ca192
Xenhat Liamano <xenhat@polarityviewer.org>
2017-06-28 23:34:17 -0400

    Temporarily disable Fullbright toggle

--- indra/llprimitive/llprimitive.cpp
+++ indra/llprimitive/llprimitive.cpp
@@ -42,8 +42,10 @@
 #include "llprimtexturelist.h"
 #include "llmaterialid.h"
 
+#ifdef FULLBRIGHT_TOGGLE
 //BD - Disable Fullbrights
 #include "..\newview\llviewercontrol.h"
+#endif
 
 /**
  * exported constants
@@ -194,17 +196,17 @@ void LLPrimitive::clearTextureList()
 // static
 LLPrimitive *LLPrimitive::createPrimitive(LLPCode p_code)
 {
-	LLPrimitive *retval = new LLPrimitive();
-	
-	if (retval)
+	LLPrimitive *retval = nullptr;
+	try
 	{
-		retval->init_primitive(p_code);
+		retval = new LLPrimitive();
 	}
-	else
+	catch (const std::bad_alloc& e)
 	{
-		LL_ERRS() << "primitive allocation failed" << LL_ENDL;
+		LL_ERRS() << "Primitive allocation failed with exception: " << e.what() << LL_ENDL;
 	}
 
+	retval->init_primitive(p_code);
 	return retval;
 }
 
@@ -1159,9 +1161,13 @@ BOOL LLPrimitive::packTEMessage(LLMessageSystem *mesgsys) const
 			offset_s[face_index] = (S16) ll_round((llclamp(te->mOffsetS,-1.0f,1.0f) * (F32)0x7FFF)) ;
 			offset_t[face_index] = (S16) ll_round((llclamp(te->mOffsetT,-1.0f,1.0f) * (F32)0x7FFF)) ;
 			image_rot[face_index] = (S16) ll_round(((fmod(te->mRotation, F_TWO_PI)/F_TWO_PI) * TEXTURE_ROTATION_PACK_FACTOR));
+#ifdef FULLBRIGHT_TOGGLE			
 //			//BD - Only allow fullbrights if we wish to see them.
 			static LLCachedControl<bool> disable_fullbright(gSavedSettings, "PVRender_DisableFullbright");
 			bump[face_index] = !disable_fullbright ? te->getBumpShinyFullbright() : te->getBumpShiny();
+#else
+			bump[face_index] = te->getBumpShinyFullbright();
+#endif
 			media_flags[face_index] = te->getMediaTexGen();
 			glow[face_index] = (U8) ll_round((llclamp(te->getGlow(), 0.0f, 1.0f) * (F32)0xFF));
 
@@ -1246,9 +1252,13 @@ BOOL LLPrimitive::packTEMessage(LLDataPacker &dp) const
 			offset_s[face_index] = (S16) ll_round((llclamp(te->mOffsetS,-1.0f,1.0f) * (F32)0x7FFF)) ;
 			offset_t[face_index] = (S16) ll_round((llclamp(te->mOffsetT,-1.0f,1.0f) * (F32)0x7FFF)) ;
 			image_rot[face_index] = (S16) ll_round(((fmod(te->mRotation, F_TWO_PI)/F_TWO_PI) * TEXTURE_ROTATION_PACK_FACTOR));
+#ifdef FULLBRIGHT_TOGGLE
 //			//BD - Only allow fullbrights if we wish to see them.
 			static LLCachedControl<bool> disable_fullbright(gSavedSettings, "PVRender_DisableFullbright");
 			bump[face_index] = !disable_fullbright ? te->getBumpShinyFullbright() : te->getBumpShiny();
+#else
+			bump[face_index] = te->getBumpShinyFullbright();
+#endif
 			media_flags[face_index] = te->getMediaTexGen();
             glow[face_index] = (U8) ll_round((llclamp(te->getGlow(), 0.0f, 1.0f) * (F32)0xFF));
 
@@ -1369,9 +1379,13 @@ S32 LLPrimitive::applyParsedTEMessage(LLTEContents& tec)
 		retval |= setTEScale(i, tec.scale_s[i], tec.scale_t[i]);
 		retval |= setTEOffset(i, (F32)tec.offset_s[i] / (F32)0x7FFF, (F32) tec.offset_t[i] / (F32) 0x7FFF);
 		retval |= setTERotation(i, ((F32)tec.image_rot[i] / TEXTURE_ROTATION_PACK_FACTOR) * F_TWO_PI);
+#ifdef FULLBRIGHT_TOGGLE
 //		//BD - Only allow fullbrights if we wish to see them.
 		static LLCachedControl<bool> disable_fullbright(gSavedSettings, "PVRender_DisableFullbright");
 		retval |= (!disable_fullbright ? setTEBumpShinyFullbright(i, tec.bump[i]) : setTEBumpShiny(i, tec.bump[i]));
+#else
+		retval |= setTEBumpShinyFullbright(i, tec.bump[i]);
+#endif
 		retval |= setTEMediaTexGen(i, tec.media_flags[i]);
 		retval |= setTEGlow(i, (F32)tec.glow[i] / (F32)0xFF);
 		retval |= setTEMaterialID(i, tec.material_ids[i]);
@@ -1489,9 +1503,13 @@ S32 LLPrimitive::unpackTEMessage(LLDataPacker &dp)
 		retval |= setTEScale(i, scale_s[i], scale_t[i]);
 		retval |= setTEOffset(i, (F32)offset_s[i] / (F32)0x7FFF, (F32) offset_t[i] / (F32) 0x7FFF);
 		retval |= setTERotation(i, ((F32)image_rot[i] / TEXTURE_ROTATION_PACK_FACTOR) * F_TWO_PI);
+#ifdef FULLBRIGHT_TOGGLE
 //		//BD - Only allow fullbrights if we wish to see them.
 		static LLCachedControl<bool> disable_fullbright(gSavedSettings, "PVRender_DisableFullbright");
 		retval |= (!disable_fullbright ? setTEBumpShinyFullbright(i, bump[i]) : setTEBumpShiny(i, bump[i]));
+#else
+		retval |= setTEBumpShinyFullbright(i, bump[i]);
+#endif
 		retval |= setTEMediaTexGen(i, media_flags[i]);
 		retval |= setTEGlow(i, (F32)glow[i] / (F32)0xFF);
 		retval |= setTEMaterialID(i, material_ids[i]);
