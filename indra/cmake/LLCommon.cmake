b98263bdb451c6326b7ffe9eff5eb7bab75c6ec2
Xenhat Liamano <xenhat@polarityviewer.org>
2017-07-27 14:49:20 -0400

    diff shenanigans

--- indra/cmake/LLCommon.cmake
+++ indra/cmake/LLCommon.cmake
@@ -17,16 +17,16 @@ set(LLCOMMON_SYSTEM_INCLUDE_DIRS
     ${Boost_INCLUDE_DIRS}
     )
 
-    # In order to support using ld.gold on linux, we need to explicitely
-    # specify all libraries that llcommon uses.
-    # llcommon uses `clock_gettime' which is provided by librt on linux.
-    set(LLCOMMON_LIBRARIES llcommon 
-        ${BOOST_COROUTINE_LIBRARY} 
-        ${BOOST_CONTEXT_LIBRARY} 
-        ${BOOST_THREAD_LIBRARY} 
-        ${BOOST_SYSTEM_LIBRARY} 
+# In order to support using ld.gold on linux, we need to explicitely
+# specify all libraries that llcommon uses.
+# llcommon uses `clock_gettime' which is provided by librt on linux.
+set(LLCOMMON_LIBRARIES llcommon 
+    ${BOOST_COROUTINE_LIBRARY} 
+    ${BOOST_CONTEXT_LIBRARY} 
+    ${BOOST_THREAD_LIBRARY} 
+    ${BOOST_SYSTEM_LIBRARY} 
     ${RT_LIBRARY}
-        )
+    )
 
 add_definitions(${TCMALLOC_FLAG})
 
